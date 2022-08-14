e917aee899ff90c616d70cdf8b1eaf8aca701420
unknown <gibranoderenz123@gmail.com>
2022-08-14 17:38:50 +0700

    Modify footer

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -2,7 +2,7 @@ import { Logo } from "../components";
 
 const Footer = () => {
   return (
-    <div className="p-16 flex items-center justify-between">
+    <div className="p-16 flex items-center justify-between bg-slate-200 border-t-2 border-black">
       <div>
         <Logo />
         <p className="text-xs mt-3">© Team 1 Compfest 14</p>
