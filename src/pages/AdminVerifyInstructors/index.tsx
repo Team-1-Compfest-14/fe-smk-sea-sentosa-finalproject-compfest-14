e377ea1c8f00220a704c360fecabce0fef5b5fa8
Fajar <fajarmh2@gmail.com>
2022-08-16 23:18:29 +0700

    comments refreshToken

--- src/pages/AdminVerifyInstructors/index.tsx
+++ src/pages/AdminVerifyInstructors/index.tsx
@@ -3,20 +3,18 @@ import jwt_decode from "jwt-decode";
 import React, { useEffect, useState } from "react";
 
 function AdminVerifyInstructors() {
-  const [instructor, setInstructor] = useState([]);
+  const [instructors, setInstructors] = useState([]);
   const [token, setToken] = useState("");
   const [expire, setExpire]: any = useState("");
-  const [roleUser, setRoleUser] = useState(null);
 
-  const refreshToken = async () => {
-    const response = await axios.get("http://localhost:8080/token");
-    const { accessToken } = response.data.data;
-    setToken(accessToken);
-    const decoded = jwt_decode(accessToken);
-    const { role, exp }: any = decoded;
-    setRoleUser(role);
-    setExpire(exp);
-  };
+  // const refreshToken = async () => {
+  //   const response = await axios.get("http://localhost:8080/auth/refresh");
+  //   const { accessToken } = response.data.data;
+  //   setToken(accessToken);
+  //   const decoded = jwt_decode(accessToken);
+  //   const { exp }: any = decoded;
+  //   setExpire(exp);
+  // };
 
   const axiosJWT = axios.create();
   axiosJWT.interceptors.request.use(
@@ -24,13 +22,12 @@ function AdminVerifyInstructors() {
       const currentDate = new Date();
       if (expire * 1000 < currentDate.getTime()) {
         // expired access token
-        const response = await axios.get("http://localhost:8080/token");
+        const response = await axios.post("http://localhost:8080/auth/refresh");
         const { accessToken } = response.data.data;
         config.headers.Authorization = `Bearer ${accessToken}`;
         setToken(accessToken);
         const decoded = jwt_decode(accessToken);
-        const { role, exp }: any = decoded;
-        setRoleUser(role);
+        const { exp }: any = decoded;
         setExpire(exp);
       }
       return config;
@@ -40,11 +37,36 @@ function AdminVerifyInstructors() {
     }
   );
 
-  useEffect(() => {
-    refreshToken();
+  const getUnverifiedInstructor = async () => {
+    const response = await axiosJWT.get("http://localhost:8080/approval/users", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    const { users } = response.data.data;
+    setInstructors(users);
+    console.log(instructors);
+  };
+
+  const useEffectAsync = (effect: any, inputs: any) => {
+    useEffect(() => {
+      effect();
+    }, inputs);
+  };
+
+  useEffectAsync(async () => {
+    await getUnverifiedInstructor();
   }, []);
 
-  return <div>AdminVerifyInstructors</div>;
+  return (
+    <>
+      <button onClick={getUnverifiedInstructor}>Click us</button>
+      {instructors.map((instructor: any, index: any) => {
+        <h1>{instructor.email}</h1>;
+        <h1>{index}</h1>;
+      })}
+    </>
+  );
 }
 
 export default AdminVerifyInstructors;
