85ea03a58dfdbd67bea6ebd8ecfd82ef5f3aceda
Fajar <fajarmh2@gmail.com>
2022-08-16 22:43:41 +0700

    add refresh token and axiosJWT

--- src/pages/AdminVerifyInstructors/index.tsx
+++ src/pages/AdminVerifyInstructors/index.tsx
@@ -1,6 +1,49 @@
-import React from "react";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+import React, { useEffect, useState } from "react";
 
 function AdminVerifyInstructors() {
+  const [instructor, setInstructor] = useState([]);
+  const [token, setToken] = useState("");
+  const [expire, setExpire]: any = useState("");
+  const [roleUser, setRoleUser] = useState(null);
+
+  const refreshToken = async () => {
+    const response = await axios.get("http://localhost:8080/token");
+    const { accessToken } = response.data.data;
+    setToken(accessToken);
+    const decoded = jwt_decode(accessToken);
+    const { role, exp }: any = decoded;
+    setRoleUser(role);
+    setExpire(exp);
+  };
+
+  const axiosJWT = axios.create();
+  axiosJWT.interceptors.request.use(
+    async (config: any) => {
+      const currentDate = new Date();
+      if (expire * 1000 < currentDate.getTime()) {
+        // expired access token
+        const response = await axios.get("http://localhost:8080/token");
+        const { accessToken } = response.data.data;
+        config.headers.Authorization = `Bearer ${accessToken}`;
+        setToken(accessToken);
+        const decoded = jwt_decode(accessToken);
+        const { role, exp }: any = decoded;
+        setRoleUser(role);
+        setExpire(exp);
+      }
+      return config;
+    },
+    (err) => {
+      return Promise.reject(err);
+    }
+  );
+
+  useEffect(() => {
+    refreshToken();
+  }, []);
+
   return <div>AdminVerifyInstructors</div>;
 }
 
