9b04fa5ff849bcf387bbf28980c26dae291c6413
Fajar <fajarmh2@gmail.com>
2022-08-20 00:03:55 +0700

    design and hit post api for approval new instructo

--- src/pages/AdminVerifyInstructors/index.tsx
+++ src/pages/AdminVerifyInstructors/index.tsx
@@ -41,6 +41,26 @@ function AdminVerifyInstructors() {
     setLoading(false);
   };
 
+  const approveHandlingData = async (event: any, userId: any, status: any) => {
+    console.log("hello");
+    await axiosJWT.post(
+      `http://localhost:8080/approval/users/${userId}`,
+      { approved: status },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+    );
+    const response = await axiosJWT.get("http://localhost:8080/approval/users", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    const { users } = response.data.data;
+    setInstructors(users);
+  };
+
   useEffect(() => {
     getUnverifiedInstructor();
   }, []);
@@ -50,16 +70,37 @@ function AdminVerifyInstructors() {
       <h1 className="bg-green p-10 text mt-9 w-5/6 m-auto text-black font-bold text-3xl rounded-md border-black border-2">
         Pending Instructors
       </h1>
-      {/* <button onClick={getUnverifiedInstructor}>Click us</button> */}
+
+      {instructors === [] && <h1 className="text-center">Not Data Founds</h1>}
+
       {loading ? (
-        <h1>kontol</h1>
+        <h1 className="text-center">Loading...</h1>
       ) : (
-        instructors.map((instructor: any, index: any) => {
+        instructors.map((instructor: any) => {
           return (
             <>
-              <h1>{instructor.email}</h1>
-              <h1>{index}</h1>
-              <h1>KONTOL BADAG</h1>
+              <div className="bg-white border border-black rounded-xl px-8 py-2 flex items-center justify-between mt-8 w-4/6 m-auto mb-8">
+                <div className="my-auto flex justify-between items-center">
+                  <div className="ml-6">
+                    <p className="text-2xl font-bold">{instructor.email}</p>
+                    <p className="flex items-center">{instructor.name}</p>
+                  </div>
+                </div>
+                <div className="flex justify-end mr-6">
+                  <button
+                    className="bg-blue hover:bg-gray-500 text-white font-bold py-2 px-4 rounded mr-6"
+                    onClick={(e) => approveHandlingData(e, instructor.id, true)}
+                  >
+                    Approve
+                  </button>
+                  <button
+                    className="bg-red-500 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded"
+                    onClick={(e) => approveHandlingData(e, instructor.id, false)}
+                  >
+                    Reject
+                  </button>
+                </div>
+              </div>
             </>
           );
         })
