d253102bda51e8c242d2b3eb06eb0138680ef398
unknown <gibranoderenz123@gmail.com>
2022-08-21 00:39:05 +0700

    Testing base URL

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import axios from "axios";
 
-export const BASE_URL = "https://sea-team-1.herokuapp.com";
+export const BASE_URL = "http://localhost:5000";
 
 // Based on https://www.npmjs.com/package/axios-auth-refresh
 export const refreshAuthLogic = (failedRequest: any) =>
